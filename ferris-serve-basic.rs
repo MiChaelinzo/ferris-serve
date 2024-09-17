ae9720fee7f489a8246567e9425685026bdcac5f
Michael G. Inso <68110223+MiChaelinzo@users.noreply.github.com>
2024-09-17 11:48:19 +0300

    Update ferris-serve-basic.rs

--- ferris-serve-basic.rs
+++ ferris-serve-basic.rs
@@ -22,21 +22,21 @@ fn handle_connection(mut stream: TcpStream, public_path: &Path) {
     let get_file = b"GET /";
 
     let (status_line, filename) = if buffer.starts_with(get_index) {
-        ("HTTP/1.1 200 OK\r\n\r\n", "index.html")
+        ("HTTP/1.1 200 OK\r\n\r\n", public_path.join("index.html")) 
     } else if buffer.starts_with(get_file) {
         let path = String::from_utf8_lossy(&buffer[5..]).split_whitespace().next().unwrap();
         let filepath = public_path.join(path);
         if filepath.is_file() {
-            ("HTTP/1.1 200 OK\r\n\r\n", filepath.to_str().unwrap())
+            ("HTTP/1.1 200 OK\r\n\r\n", filepath) 
         } else {
-            ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
+            ("HTTP/1.1 404 NOT FOUND\r\n\r\n", public_path.join("404.html")) 
         }
     } else {
-        ("HTTP/1.1 400 BAD REQUEST\r\n\r\n", "400.html")
+        ("HTTP/1.1 400 BAD REQUEST\r\n\r\n", public_path.join("400.html")) 
     };
 
     let contents = fs::read_to_string(filename).unwrap_or_else(|_| {
-        format!("Error reading file: {}", filename)
+        format!("Error reading file") 
     });
 
     let response = format!("{}{}", status_line, contents);
